e3ce7f6f558fa5bd457ad949c99cecf471101873
Faruk Yucel <farukyucel5@gmail.com>
2022-11-12 11:22:03 +0300

    replace

--- src/ders11/replace.java
+++ src/ders11/replace.java
@@ -4,8 +4,11 @@ public class replace {
     public static void main(String[] args) {
         String input = "ja5+va cok 1*guzel";
         input = input.replaceAll("\\d", "");
+        System.out.println(input);
         input = input.replaceAll("\\s", "5");
+        System.out.println(input);
         input = input.replaceAll("\\W", "");
+        System.out.println(input);
         input = input.replaceAll("\\d", " ");
 
 
